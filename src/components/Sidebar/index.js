b29780ff1eaf17f08afcda65dbadca82d61a5b4d
P_WebDew <75292399+PVtig@users.noreply.github.com>
2021-03-29 16:39:40 +0300

    Add files via upload

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import CustomApp from '../CustomPages/CustomApp';
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+  SideBtnWrap,
+} from './SidebarElements';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarMenu>
+        <SidebarLink to="/">Pizzas</SidebarLink>
+        <SidebarLink to="/">Desserts</SidebarLink>
+        <SidebarLink to="/">Full Menu</SidebarLink>
+        <SidebarLink to="/custom">Custom Pizzas</SidebarLink>
+      </SidebarMenu>
+      <Router>
+        <Switch>
+          <Route path="/custom">
+            <CustomApp />
+          </Route>
+          {/* <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route> */}
+        </Switch>
+      </Router>
+      <SideBtnWrap>
+        <SidebarRoute to="/">Order Now</SidebarRoute>
+      </SideBtnWrap>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
